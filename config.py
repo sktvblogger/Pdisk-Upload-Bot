b501b326a9960c4f29f0dc57f943a2a919a9cbce
Nikita Sharma <123251896+sktvblogger@users.noreply.github.com>
2023-04-22 08:00:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6027500067:AAG-veKTUgl3nSIT2rpiK_FMAWv_60vPDqg")
 
-    API_ID = int(os.environ.get("API_ID", 123456))
+    API_ID = int(os.environ.get("API_ID", "4546803"))
 
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "08ad181fba3b05e1141db96175cab60e")
     
-    API_KEY = os.environ.get("API_KEY", "")
+    API_KEY = os.environ.get("API_KEY", "99xz8zhn3shdv021ws'")
 
     # AUTH_USERS = set(str(x) for x in os.environ.get("AUTH_USERS", "").split())
 
